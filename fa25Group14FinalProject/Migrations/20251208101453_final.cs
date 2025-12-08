c8ac5a6700138e7b2195c67762f94f75266a2a80
Noah Yu <noaheyu@utexas.edu>
2025-12-08 05:00:36 -0600

    final migration

---
+++ fa25Group14FinalProject/Migrations/20251208101453_final.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fa25Group14FinalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
